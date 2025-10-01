f9e7159f8eab4935c7e5c3efdc5af3224d283e4c
Manas Kashyap <manaskashyap1372@gmail.com>
2025-10-01 05:49:45 +0000

    mathFunction explanation

---
+++ javaScript-basics/numberMath.js
@@ -0,0 +1,35 @@
+const score=400
+// console.log(typeof score);
+const balance=new Number(100)
+// console.log(typeof balance);
+
+// console.log(balance);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(3));
+const otherNumber=123.9567
+// console.log(typeof otherNumber);
+
+// console.log(otherNumber.toPrecision(3));
+// console.log(score.toString());
+// console.log(typeof score);
+
+const anotherNumber=Number(420)
+// console.log(typeof anotherNumber);
+
+
+const hundereds=1000000
+// console.log(hundereds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++++++++Math++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(34.8));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(1,4,67,98900,0,-5));
+// console.log(Math.max(1,4,67,98900,0,-5));
+
+
+
+console.log(Math.round(Math.random()*10+1));
